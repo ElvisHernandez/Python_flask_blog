3469278ce38c3f460ed531bccb10477d1ed28400
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-06-04 08:41:32 -0400

    added flask-pagedown, markdown, and bleach extensions

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,14 @@ from flask_moment import Moment
 from config import config
 from threading import Thread
 from flask_login import LoginManager
+from flask_pagedown import PageDown
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+pagedown = PageDown()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,6 +21,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     login_manager.init_app(app)
+    pagedown.init_app(app)
 
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
